9f62c4595a023084f0badc241c392325baed1e92
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-19 10:12:05 -0700

    Updating

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_rds_cluster" "default" {
   master_username         = "foo"
   master_password         = "bar"
   skip_final_snapshot  = true
-  db_cluster_parameter_group_name = "${var.env}-${var.name}-roboshop-rds"
+ // db_cluster_parameter_group_name = "${var.env}-${var.name}-roboshop-rds"
   db_subnet_group_name = "${var.env}-${var.name}-roboshop-rds"
 }
 
@@ -16,6 +16,7 @@ resource "aws_rds_cluster_parameter_group" "default" {
   family      = "aurora5.6"
   description = "RDS default cluster parameter group"
 }
+
 
 resource "aws_db_subnet_group" "default" {
   name       = "${var.env}-${var.name}-roboshop-rds"
