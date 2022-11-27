e5789e96a02128366fa788ca46c8db68b5051cb5
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-26 17:10:34 -0700

    Updating

--- schema.tf
+++ schema.tf
@@ -1,17 +1,14 @@
-
-
 resource "null_resource" "load-schema" {
   depends_on = [aws_rds_cluster.default]
 
   provisioner "local-exec" {
     command = <<EOF
+sleep 60
 curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip"
 cd /tmp
 unzip -o mysql.zip
 cd mysql-main
-mysql -h ${aws_rds_cluster.default.endpoint} - u${local.REDIS_USER} -p${local.REDIS_PASS} <shipping.sql
+mysql -h ${aws_rds_cluster.default.endpoint} -u${local.REDIS_USER} -p${local.REDIS_PASS} <shipping.sql
 EOF
   }
-
 }
-
