20c79551d5d94656652271d7c0540e4300f84193
Ayodeji61 <alabidejimichael@gmail.com>
2022-11-26 17:06:56 -0700

    Updating

--- schema.tf
+++ schema.tf
@@ -9,7 +9,7 @@ curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/a
 cd /tmp
 unzip -o mysql.zip
 cd mysql-main
-
+mysql -h ${aws_rds_cluster.default.endpoint} - u${local.REDIS_USER} -p${local.REDIS_PASS} <shipping.sql
 EOF
   }
 
